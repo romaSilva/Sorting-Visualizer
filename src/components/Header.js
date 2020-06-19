42ea8d5af7d30abc7303883ce1b566b706b69cbf
Romario Da Silva <roma.dasilva94@gmail.com>
2020-06-19 17:15:51 -0300

    all ui done

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,47 +15,57 @@ const Header = () => {
 
   return (
     <div className="header-container">
-      <button
-        style={sorting ? { color: "#7A7A7A" } : {}}
-        onClick={() => {
-          if (!sorting) {
-            handleNewArray();
-          }
-        }}
-      >
-        Generate New Array
-      </button>
-      <button
-        style={sorting !== "quick" && sorting ? { color: "#7A7A7A" } : {}}
-        onClick={() => {
-          if (!sorting) {
-            handleSorting();
-            handleQuickSort();
-          }
-        }}
-      >
-        Quick Sort
-      </button>
-      <button
-        style={sorting !== "bubble" && sorting ? { color: "#7A7A7A" } : {}}
-      >
-        Bubble Sort
-      </button>
-      <button style={sorting !== "heap" && sorting ? { color: "#7A7A7A" } : {}}>
-        Heap Sort
-      </button>
-      <button
-        style={sorting !== "merge" && sorting ? { color: "#7A7A7A" } : {}}
-      >
-        Merge Sort
-      </button>
-      <button
-        id="stop"
-        style={cancelBtnStyle}
-        onClick={() => window.location.reload()}
-      >
-        <AiOutlineStop />
-      </button>
+      <div className="btn-container">
+        <button
+          style={sorting ? { color: "#7A7A7A" } : {}}
+          onClick={() => {
+            if (!sorting) {
+              handleNewArray();
+            }
+          }}
+        >
+          Generate New Array
+        </button>
+      </div>
+
+      <div className="btn-container">
+        <button
+          style={sorting !== "quick" && sorting ? { color: "#7A7A7A" } : {}}
+          onClick={() => {
+            if (!sorting) {
+              handleSorting();
+              handleQuickSort();
+            }
+          }}
+        >
+          Quick Sort
+        </button>
+        <button
+          style={sorting !== "bubble" && sorting ? { color: "#7A7A7A" } : {}}
+        >
+          Bubble Sort
+        </button>
+        <button
+          style={sorting !== "heap" && sorting ? { color: "#7A7A7A" } : {}}
+        >
+          Heap Sort
+        </button>
+        <button
+          style={sorting !== "merge" && sorting ? { color: "#7A7A7A" } : {}}
+        >
+          Merge Sort
+        </button>
+      </div>
+
+      <div className="btn-container">
+        <button
+          id="stop"
+          style={cancelBtnStyle}
+          onClick={() => window.location.reload()}
+        >
+          <AiOutlineStop />
+        </button>
+      </div>
     </div>
   );
 };
